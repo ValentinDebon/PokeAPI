eda45b6cdd9f3377bcc54bf0475819e84379b633
Valentin Debon <valentin@debon.net>
2020-03-15 21:12:52 +0100

    Modified PokeAPILocal source code to ensure coherence in local files

--- Sources/PokeAPI/PokeAPILocal.swift
+++ Sources/PokeAPI/PokeAPILocal.swift
@@ -1,14 +1,16 @@
 import Foundation
 
 public final class PokeAPILocal: PokeAPIProtocol {
+	public static let indexFile = "index.json"
+
 	private let url: URL
 	private let decoder: JSONDecoder
 	private let endpoints: [String: String]
 
-	public init(at url: URL, index: String = "/api/v2/index.json") throws {
+	public init(at url: URL, index: String = "/api/v2") throws {
 		self.url = url
 		self.decoder = JSONDecoder()
-		self.endpoints = try self.decoder.decode([String: String].self, from: Data(contentsOf: url.appendingPathComponent(index)))
+		self.endpoints = try self.decoder.decode([String: String].self, from: Data(contentsOf: url.appendingPathComponent(index).appendingPathComponent(PokeAPILocal.indexFile)))
 	}
 
 	public func location(endpoint: String, id: String? = nil) -> String? {
@@ -16,15 +18,19 @@ public final class PokeAPILocal: PokeAPIProtocol {
 			return nil
 		}
 
-		return endpoint + (id ?? "/index.json")
+		if let id = id {
+			return endpoint + id
+		} else {
+			return endpoint
+		}
 	}
 
 	public func resource<R>(at location: String) -> R? where R: Resource {
-		try? self.decoder.decode(R.self, from: Data(contentsOf: self.url.appendingPathComponent(location).appendingPathComponent("index.json")))
+		try? self.decoder.decode(R.self, from: Data(contentsOf: self.url.appendingPathComponent(location).appendingPathComponent(PokeAPILocal.indexFile)))
 	}
 
 	public func locationAreaEncounters(pokemon: Pokemon) -> Set<LocationAreaEncounter>? {
-		try? self.decoder.decode(Set<LocationAreaEncounter>.self, from: Data(contentsOf: self.url.appendingPathComponent(pokemon.locationAreaEncounters).appendingPathComponent("/index.json")))
+		try? self.decoder.decode(Set<LocationAreaEncounter>.self, from: Data(contentsOf: self.url.appendingPathComponent(pokemon.locationAreaEncounters).appendingPathComponent(PokeAPILocal.indexFile)))
 	}
 
 	public func resourceList<R>() -> APIResourceList<R>? where R: Resource {
@@ -32,7 +38,7 @@ public final class PokeAPILocal: PokeAPIProtocol {
 			return nil
 		}
 
-		return try? self.decoder.decode(APIResourceList<R>.self, from: Data(contentsOf: self.url.appendingPathComponent(endpoint).appendingPathComponent("/index.json")))
+		return try? self.decoder.decode(APIResourceList<R>.self, from: Data(contentsOf: self.url.appendingPathComponent(endpoint).appendingPathComponent(PokeAPILocal.indexFile)))
 	}
 
 	public func namedResourceList<R>() -> NamedAPIResourceList<R>? where R: NamedResource {
@@ -40,7 +46,7 @@ public final class PokeAPILocal: PokeAPIProtocol {
 			return nil
 		}
 
-		return try? self.decoder.decode(NamedAPIResourceList<R>.self, from: Data(contentsOf: self.url.appendingPathComponent(endpoint).appendingPathComponent("/index.json")))
+		return try? self.decoder.decode(NamedAPIResourceList<R>.self, from: Data(contentsOf: self.url.appendingPathComponent(endpoint).appendingPathComponent(PokeAPILocal.indexFile)))
 	}
 }
 
